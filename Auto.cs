835b7d6dff0770ca843b895dbfdfad0c99414474
nadrabalazs <158039410+nadrabalazs@users.noreply.github.com>
2026-03-04 14:17:12 +0100

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoclass
+{
+    internal class Auto
+    {
+        string rendszam;
+        string tipusNev;
+        double fogyasztas; //100kmenkénti avg
+        int kmora;
+        int szervizkm;
+
+        int utolsoSzervizKm; //friss szerviz
+        double eddigiFogyasztas;
+        public Auto(string rendszam, string tipusNev, double fogyasztas, int szervizkm, int kmora = 0)
+        {
+            this.rendszam = rendszam;
+            this.tipusNev = tipusNev;
+            this.fogyasztas = fogyasztas;
+            this.kmora = kmora;
+            this.szervizkm = szervizkm;
+            this.utolsoSzervizKm = 0;
+            this.eddigiFogyasztas = 0;
+        }
+        public bool KellSzervizelni()
+        {
+            return utolsoSzervizKm > szervizkm - 500;
+        }
+
+        public void szervizElvegzese()
+        {
+            this.utolsoSzervizKm = 0;
+        }
+
+        public double FelhasznaltUzemanyag()
+        { 
+            return kmora/100*fogyasztas; // új autó eset
+        }
+
+        public string Rendszam { get => rendszam; }
+        public string TipusNev { get => tipusNev; }
+        public double Fogyasztas { get => fogyasztas; }
+        public int Kmora { get => kmora; }
+        public int Szervizkm { get => szervizkm; }
+
+        public override string? ToString()
+        {
+            return $"Rendszám: {this.rendszam}, Típusnév: {this.tipusNev}, Fogyasztás: {this.fogyasztas:F1}l/100km, km óra állása: {this.kmora} ";
+        }
+
+        public void Menj(int mennyit)
+        {
+            if(this.utolsoSzervizKm > szervizkm + 100)
+            {
+                throw new Exception("Ezt a kocsit szervizelni kell!");
+            }
+            //todo hibaellenőrzés
+            this.kmora += mennyit;
+            this.utolsoSzervizKm += mennyit;
+            this.eddigiFogyasztas += mennyit / 100 * eddigiFogyasztas;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace autoclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto skoda, passat;
+            skoda = new Auto("AB-CD-123", "Skoda SuperB", 5.23, 16000);
+            passat = new Auto("XYZ-789", "VW Passat", 6.97, 13000, 256000);
+
+            Console.WriteLine(skoda);
+            Console.WriteLine(passat);
+            for (int i = 1; i < 30; i++)
+            {
+                if (skoda.KellSzervizelni())
+                {
+                    skoda.szervizElvegzese();
+                    Console.WriteLine("SZERVIZ!");
+                }
+                skoda.Menj(1000);
+                Console.WriteLine($"{i+1}.{skoda}");
+            }
+        }
+    }
+}
